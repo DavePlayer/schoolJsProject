202f8ae93acb4be5d7764edaa7d02cc0fd739078
unknown <daveplayer2001@gmail.com>
2020-04-04 20:49:00 +0200

    created summary for trolley and stylized rest

--- src/components/productsTable.tsx
+++ src/components/productsTable.tsx
@@ -20,27 +20,28 @@ export const ProductsTable: React.FC = () => {
 
     return (
         <>
-        <table>
-            <thead>
-            <tr className="tableHeader">
-                <th>LP.</th>
-                <th>Nazwa</th>
-                <th>Cena</th>
-                <th>Stan</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-                {products.length > 0?
-                    products.map( (obj, index) => {
-                        return <Product inTrolley={false} key={index} index={index} data={obj} />
-                    })
-                    :
-                    <></>
-                }
-            </tbody>
-        </table>
-        <button onClick={() => dispatch(isAdding())} className="addProduct">Dodaj produkt</button>
+            <h3>Towary</h3>
+            <table>
+                <thead>
+                <tr className="tableHeader">
+                    <th>LP.</th>
+                    <th>Nazwa</th>
+                    <th>Cena</th>
+                    <th>Stan</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                    {products.length > 0?
+                        products.map( (obj, index) => {
+                            return <Product inTrolley={false} key={index} index={index} data={obj} />
+                        })
+                        :
+                        <></>
+                    }
+                </tbody>
+            </table>
+            <button onClick={() => dispatch(isAdding())} className="addProduct">Dodaj produkt</button>
         </>
     )
 }
--- src/components/trolleyTable.tsx
+++ src/components/trolleyTable.tsx
@@ -1,34 +1,51 @@
 import * as React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { Istate } from './reducers/combined'
 import { Product } from './product'
-import { useEffect } from 'react'
-import { loadTrolleyFromLocalStorage } from './actions/loadTrolleyFromLocalStorage'
+import { useState, useEffect } from 'react'
 
 export const TrolleyTable:React.FC = () => {
     const trolley = useSelector((state:Istate) => state.productsInTrolley)
+    const [summary, setSummary] = useState(0)
+
+    useEffect(() => {
+        setSummary(0)
+        trolley.map(o => {
+            setSummary(sum => sum+=o.price)
+        })
+    }, [trolley.length])
     
     console.log("trolley", trolley)
     return (
-        <table className="trolley">
-            <thead>
-            <tr className="tableHeader">
-                <th>LP.</th>
-                <th>Nazwa</th>
-                <th>Cena</th>
-                <th>Stan</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-                {trolley.length > 0?
-                    trolley.map( (obj, index) => {
-                        return <Product inTrolley={true} key={index} index={index} data={obj} />
-                    })
-                    :
-                    <></>
-                }
-            </tbody>
-        </table>
+        <>
+            <h3 className="trolley">Koszyk</h3>
+            <table>
+                <thead>
+                <tr className="tableHeader">
+                    <th>LP.</th>
+                    <th>Nazwa</th>
+                    <th>Cena</th>
+                    <th>Stan</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                    {trolley.length > 0 ?
+                        trolley.map( (obj, index) => {
+                            return <Product inTrolley={true} key={index} index={index} data={obj} />
+                        })
+                        :
+                        <></>
+                    }
+                        <tr className="tableHeader">
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>Łącznie: {summary.toFixed(2) + ' zł'}</td>
+                        </tr>                    
+                </tbody>
+            </table>
+        </>
     )
 }
