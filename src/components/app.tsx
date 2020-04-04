e22e0dbc1b924b5ca92972111f0e3d10f6a1af24
unknown <daveplayer2001@gmail.com>
2020-04-04 21:47:47 +0200

    added and stylized footer with social links

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,6 +6,7 @@ import { useSelector } from 'react-redux'
 import { Istate } from './reducers/combined'
 import { TrolleyTable } from './trolleyTable'
 import { Header } from './header'
+import { Footer } from './footer'
 
 export const App:React.FC = () => {
     const isAddingProduct = useSelector((state:Istate) => state.isAdding)
@@ -21,6 +22,7 @@ export const App:React.FC = () => {
                 :
                 <></>
             }
+            <Footer />
         </>
     )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+export const Footer:React.FC = () => {
+    return(
+        <footer>
+            <div className="buySth">
+                <h4>Buy yourself something</h4>
+                <p>We are ready for the challenge.
+                We should provide you everything we
+                have got.</p>
+            </div>
+            <div className="ContactUs">
+                <h4>Contact US</h4>
+                <p>Cieszynki 24/100 Miasto 32-111
+                +44 666 666 666</p>
+                <p className="contactParagraph"><a href="#">or Contact Link</a></p>
+            </div>
+            <div className="CopyRigths">
+                <h4>Copyright</h4>
+                <p>&copy;Dawid Grygiel 2020 
+                All rights reserved</p>
+            </div>
+            <div className="SocialLinks">
+                <a href="#" className="fa fa-facebook"></a>
+                <a href="#" className="fa fa-twitter"></a>
+                <a href="#" className="fa fa-instagram"></a>
+            </div>
+        </footer>
+    )
+}
