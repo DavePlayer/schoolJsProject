2755b271edf4d8e87e57acaedcd0a90de26e5b4f
DavePlayer <daveplayer2001@gmail.com>
2020-05-12 13:27:53 +0200

    added shoping cart with sum of products prices to navigation (will be a link to trolley page)

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,7 +1,24 @@
 import * as React from 'react';
 import {Link} from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { Istate } from './reducers/combined';
+import { Iproducts } from './reducers/products';
+import { useState, useEffect } from 'react';
 
 export const Nav:React.FC = () => {
+    const trolleyProducts:Array<Iproducts> = useSelector((o:Istate) => o.productsInTrolley)
+    const [sum, setSum] = useState(0.00)
+
+    useEffect(() => {
+        setSum(0)
+        if(trolleyProducts.length != 0){
+            trolleyProducts.map( (o:Iproducts, i:number) => {
+                console.log('price: ',o.price)
+                setSum(prev => prev += o.price)
+            })
+        }
+    }, [trolleyProducts])
+    console.log('sum: ', sum)
     return(
         <div className="navigation">
             <h1><a href="#">Sklep</a></h1>
@@ -11,6 +28,12 @@ export const Nav:React.FC = () => {
                     <li><Link to='/schoolJsProject/shop'>Zakupy</Link></li>
                     <li><Link to='/schoolJsProject/contact'>Kontakt</Link></li>
                     <li><Link to='/schoolJsProject/admin'>Admin</Link></li>
+                    <li>
+                        <Link to='/schoolJsProject/'>
+                            <span className="material-icons">shopping_cart</span>
+                            {sum} zł
+                        </Link>
+                    </li>
                 </ul>
             </nav>
         </div>
