f03649986f9385c1a8030f84447cc11861f1d582
DavePlayer <daveplayer2001@gmail.com>
2020-05-30 21:00:52 +0200

    added product page responsivity for phones

--- src/components/tileProduct.tsx
+++ src/components/tileProduct.tsx
@@ -4,20 +4,26 @@ import { useDispatch } from 'react-redux'
 import { moveToTrolley } from './actions/moveToTrolley'
 import { useState } from 'react'
 import { deleteProduct } from './actions/deleteProduct'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 
 export const TileProduct:React.FC<Iproducts> = (props:Iproducts) => {
     const ch = Math.floor(Math.random()*4)+1
     const arr = [1,2,3,4,5]
     const dispatch = useDispatch()
+    const history = useHistory()
+    const goTopage = () => {
+        if(window.innerWidth < 993)
+        history.push(`/schoolJsProject/shop/${props.id}`)
+    }
 
     return(
     <article className="tile">
         <figure><img src={props.imgAdress} alt="product image"/></figure>
-        <section className="info">
+        <section onClick={() => goTopage()}  className="info">
             <p className="productName">{props.name}</p>
-            <p>Sprzedawca: {props.seller}</p>
+            <p className="phonePrice">Cena: {props.price}</p>
+            <p className="seller">Sprzedawca: {props.seller}</p>
             <p className="showOffs">
                     <span className="material-icons yellow">grade</span>
                     <span className="material-icons yellow">grade</span>
@@ -26,7 +32,7 @@ export const TileProduct:React.FC<Iproducts> = (props:Iproducts) => {
                     <span className="material-icons">grade</span>
                 <span>kupiono {Math.floor((Math.random()*144)+3)} sztuk</span>
             </p>
-            <ul>
+            <ul className="list">
                 <li>stan: {props.state}</li>
                 <li>typ: {props.type}</li>
             </ul>
